38d5316d0bf04bcf28c42e36da91dcbd99850c05
Jason Galea <jason@lecstor.com>
2022-10-21 16:28:17 +1000

    fix tests

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,6 @@
 /** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
 module.exports = {
   preset: "ts-jest",
+  setupFilesAfterEnv: ["<rootDir>/src/setupTests.ts"],
   testEnvironment: "jsdom",
 };
